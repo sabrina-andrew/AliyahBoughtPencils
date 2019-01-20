d0c43637819bede27147a55ecca6f49a4d002873
Sabrina Andrew <sabrina.rae.andrew@gmail.com>
2019-01-20 16:29:18 -0500

    First Commit

---
+++ source/repos/AliyahBoughtPencils/AliyahBoughtPencils/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace AliyahBoughtPencils
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Aliyah had \$24 to spend on seven pencils. After buying them she had 10. How much did each pencil cost?
+
+            int inWallet = 24;
+            int afterBuyingPencils = 10;
+            int numberOfPencils = 7;
+            int totalCostOfPencils = inWallet - afterBuyingPencils;
+            int costPerPencil = totalCostOfPencils / numberOfPencils;
+            Console.WriteLine("Aliyah spent $" + costPerPencil + " per pencil.");
+        }
+    }
+}
